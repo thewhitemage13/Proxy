cb18729f80bd182b33ce815a79594b4a8e04c975
Mukhammed Lolo <144810207+thewhitemage13@users.noreply.github.com>
2024-05-20 20:10:01 +0200

    Add files via upload

---
+++ Proxy - C++/Proxy - C++/Proxy - C++.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+class ISite {
+public:
+    virtual string GetPage(int num) = 0;
+    virtual ~ISite(){}
+};
+
+class Site : public ISite {
+public:
+    string GetPage(int num) override {
+        string page = "site page" + to_string(num);
+        return page;
+    }
+};
+
+class SiteProxy : public ISite {
+private:
+    ISite* site;
+    map<int, string> cache;
+public:
+    SiteProxy(ISite* site) : site(site){}
+    ~SiteProxy() {
+        delete site;
+    }
+
+    string GetPage(int num) override {
+        string page;
+        if (cache.find(num) == cache.end()) {
+            page = site->GetPage(num);
+            cache[num] = page;
+        }
+        else {
+            page = cache[num];
+            page.insert(0, "from: ");
+        }
+        return page;
+    }
+};
+
+int main() {
+    ISite* mysite = new SiteProxy(new Site());
+    cout << mysite->GetPage(1) << "\n";
+    cout << mysite->GetPage(2) << "\n";
+    cout << mysite->GetPage(3) << "\n";
+
+    cout << mysite->GetPage(2) << "\n";
+}
